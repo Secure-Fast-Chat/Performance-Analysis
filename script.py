3edb914a260d7ae7653dfd9feb055f2e2516b08e
Arhaan Ahmad <arhaan.ahmad2003@gmail.com>
2022-11-25 02:17:44 +0530

    Fix errors

--- script.py
+++ script.py
@@ -6,20 +6,49 @@ import time
 # os.chdir('../Server-Side')
 # s = process(['python3', 'loadbalancer.py'])
 p = []
-n=100
-for i in range(100):
+n=10
+for i in range(n):
     p.append(process(['python3','../Client_Side/app.py']))
-    p[i].sendline(b'1')
-    p[i].sendline(str(i).encode('utf-8'))
-    p[i].sendline(str(i).encode('utf-8'))
+    print(p[i].recvuntil(")\n"))
+    print(p[i].recvuntil(":"))
 
-time.sleep(5)
+    # Signup
+    # print(p[i].recvuntil(":"))
+    # p[i].sendline(b'2')
+    # print(p[i].recvuntil(":"))
+    # p[i].sendline(str(i).encode('utf-8'))
+    # print(p[i].recvuntil(":"))
+    # p[i].sendline(str(i).encode('utf-8'))
+    # print(p[i].recvuntil(":"))
+    # p[i].sendline(str(i).encode('utf-8'))
+    # Login
+    print("i###########Login###########")
+    print(p[i].recvuntil(":"))
+    p[i].sendline(b'1')
+    print(p[i].recvuntil(":"))
+    p[i].sendline(str(i).encode('utf-8'))
+    print(p[i].recvuntil(":"))
+    p[i].sendline(str(i).encode('utf-8'))
+    print(p[i].recvuntil("In"))
+
+time.sleep(2)
+
 print('reached')
-for i in range(100):
+for i in range(n):
     try:
-        p[i].sendline(b'\send 0 '+ str(datetime.datetime.timestamp(datetime.datetime.now())).encode('utf-8'))
-    except:
+        print(p[i].recv())
+        sendMsgCommand = '\send 0 '+ str(datetime.datetime.timestamp(datetime.datetime.now()))
+        print(sendMsgCommand)
+        p[i].sendline(sendMsgCommand.encode('utf-8'))
+        # p[0].recv()
+    except Exception as e:
         print(i)
+        print(e)
         continue
 
 time.sleep(5)
+for i in range(n):
+    print("Logging out")
+    logout = "\logout"
+    p[i].sendline(logout.encode('utf-8'))
+

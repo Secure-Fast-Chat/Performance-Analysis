cb2232182acb5bdd9c6c3ea0b33a75ff861a64b1
Khushang Singla <khushangsingla@gmail.com>
2022-11-25 18:18:58 +0530

    Throughput

---
+++ throuputcalculation.sh
@@ -0,0 +1,3 @@
+echo -n "Input ThroughPut = "
+cat SecureFastChatlogs_* |grep Rec | cut -d ':' -f 1| awk '{if($1>_____ && $1 < _____){print($1)}}' | wc -l
+cat SecureFastChatlogs_* |grep Sent | cut -d ':' -f 1| awk '{if($1>_____ && $1 < _____){print($1)}}' | wc -l
